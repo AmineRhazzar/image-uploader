ffe0fce2c8ce2850017fb440f8c35e0c1477f6b2
Amine <mohammedelaminerhazzar@gmail.com>
2021-05-29 19:23:31 +0100

    fixed user email on git config

--- git-fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git filter-branch -f --env-filter "
-    GIT_AUTHOR_NAME='Mohammed El Amine'
-    GIT_AUTHOR_EMAIL='mohammedelaminerhazzar@gmail.com'
-    GIT_COMMITTER_NAME='Mohammed El Amine'
-    GIT_COMMITTER_EMAIL='mohammedelaminerhazzar@gmail.com'
-  " HEAD
